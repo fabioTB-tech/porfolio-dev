bdc1fafb61438dbd1c95224aebcb75334649f99f
fabioTB-tech <drawingfabio@gmail.com>
2026-03-02 23:50:00 -0300

    habilitado para envio de E-mail

--- src/js/index.js
+++ src/js/index.js
@@ -8,10 +8,8 @@ window.addEventListener('scroll', () => {
 
     sections.forEach(section => {
         const sectionTop = section.offsetTop - headerHeight - 150
-        console.log(section)
         if (scrollY >= sectionTop) {
             current = section.getAttribute('id')
-            console.log(current)
         }
     });
 
@@ -37,3 +35,55 @@ navLink.forEach(link => {
     }
   });
 });
+
+emailjs.init({
+    publicKey: "Sc29MNP2ZLGBRdNRR"
+})
+
+document.getElementById('contact-form').addEventListener('submit', event => {
+    event.preventDefault()
+
+    const formData = {
+        name: document.getElementById('nome').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('assunto').value,
+        message: document.getElementById('msg').value
+    }
+
+    const serviceID = "service_eb1mj8a"
+    const templateID = "template_roo5aal"
+    const button = document.getElementById('enviar')
+    button.textContent = 'Enviando...'
+
+    emailjs.send(serviceID, templateID, formData).then(() => {
+        Toastify({
+
+            text: "E-mail enviado com sucesso!",
+            duration: 3000,
+            style: {
+                background: "#28a745",
+                color: "#f4f4f4"
+            }
+
+        }).showToast();
+
+        document.getElementById('contact-form').reset()
+    })
+    .catch(error => {
+        Toastify({
+
+            text: "Erro ao enviar o e-mail!",
+            duration: 3000,
+            style: {
+                background: "#dc3545",
+                color: "#f4f4f4"
+            }
+
+        }).showToast();
+
+        console.error('Erro no envio', error)
+    })
+    .finally(() => {
+        button.textContent = 'Enviar'
+    })
+})
